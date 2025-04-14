eba07c9cbcb13029b810379e5cf2db56bd5058df
Justy565 <tajjustin@gmail.com>
2025-04-14 05:42:30 +0000

    Popravlen main

--- main.py
+++ main.py
@@ -1,64 +1,63 @@
-from flask import Flask, render_template, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask import Flask, render_template, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+from tinydb import TinyDB, Query
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'thisisasecretkey'
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'secretkey'
+
+
+db = TinyDB('db.json')
+user_table = db.table('users')
+
 bcrypt = Bcrypt(app)
-
-
-
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 
+class User(UserMixin):
+    def __init__(self, user_id, username, password):
+        self.id = user_id
+        self.username = username
+        self.password = password
+
+
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+    UserQuery = Query()
+    user = user_table.get(UserQuery.id == int(user_id))
+    if user:
+        return User(user_id=user['id'], username=user['username'], password=user['password'])
+    return None
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={"placeholder": "Enter username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)],
+                             render_kw={"placeholder": "Enter password"})
     submit = SubmitField('Register')
 
+    
     def validate_username(self, username):
-        existing_user_username = User.query.filter_by(
-            username=username.data).first()
-        if existing_user_username:
-            raise ValidationError(
-                'That username already exists. Please choose a different one.')
+        existing_user = user_table.search(Query().username == username.data)
+        if existing_user:
+            raise ValidationError('Ta username je ze v uporabi')
 
 
 class LoginForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={"placeholder": "Vnesi username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)],
+                             render_kw={"placeholder": "Vnesi geslo"})
     submit = SubmitField('Login')
 
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
